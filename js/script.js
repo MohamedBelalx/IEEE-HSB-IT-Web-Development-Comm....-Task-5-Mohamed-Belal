77c362b4bba63e70e8b9b36f44ccaf0b20c52f21
MohamedBelalx <hero.armna@gmail.com>
2018-10-26 13:10:51 +0200

    last

--- js/script.js
+++ js/script.js
@@ -1,20 +1,10 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
-});
-
-/*$(document).ready(function(){
-	$('#6').on('click', function (){
-		$('#6').addClass('active');
-		$('#1').removeClass('active');
-		$('#1').on('click', function(){
-		$('#6').removeClass('active');
-		$('#1').addClass('active');
-		});
-	});
 });*/
+
 $('#sidebar li').click(function() {
     $(this).siblings('li').removeClass('active');
     $(this).addClass('active');
